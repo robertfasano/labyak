2c47f13adb0b172e050862864cb007d6bc2ed78d
Robbie Fasano <robert.j.fasano@colorado.edu>
2019-08-22 17:03:41 -0600

    Add PWM function

--- labyak/labjack_core.py
+++ labyak/labjack_core.py
@@ -43,5 +43,35 @@ class LabJack():
         except:
             pass
 
+    def PWM(self, channel, frequency, duty_cycle):
+        ''' Starts pulse width modulation on an FIO channel.
+
+            Args:
+                channel (int): FIO channel to use (0 or 2-5).
+                frequency (float): desired frequency in Hz
+                duty_cycle (float): duty cycle between 0 and 100.
+        '''
+        try:
+            roll_value = self.clock / frequency
+
+            config = {
+                "DIO_EF_CLOCK0_ENABLE": 0,
+                "DIO_EF_CLOCK0_DIVISOR": 1,
+                "DIO_EF_CLOCK0_ROLL_VALUE": roll_value,
+                "DIO_EF_CLOCK0_ENABLE": 1,
+                "DIO%i_EF_ENABLE"%channel: 0,
+                "DIO%i_EF_INDEX"%channel: 0,
+                "DIO%i_EF_OPTIONS"%channel: 0,
+                "DIO%i_EF_CONFIG_A"%channel: duty_cycle * roll_value / 100,
+                "DIO%i_EF_ENABLE"%channel: 1
+            }
+            self._write_dict(config)
+
+        except Exception as e:
+            print(e)
+
+    def PWM_stop(self, channel):
+        self._command("DIO%i_EF_ENABLE"%channel, 0)
+
 if __name__ == '__main__':
-    lj = LabJack(devid='470018954')
+    lj = LabJack(devid='470018953')
