2bc039b9a0f73485f831c50657c9479d108bbf8d
Robbie Fasano <robert.j.fasano@colorado.edu>
2019-08-22 17:04:04 -0600

    Import new SPIDriver and TemperatureSensor classes

--- labyak/__init__.py
+++ labyak/__init__.py
@@ -2,3 +2,5 @@ from .labjack_core import LabJack
 from .pattern_generator import PatternGenerator
 from .waveform_generator import WaveformGenerator
 from .daq import LabDAQ
+from .temperature_sensor import TemperatureSensor
+from .spi_driver import SPIDriver
